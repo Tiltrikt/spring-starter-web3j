85769848b87e39657d3bca69bfcba382eacf337c
Tiltrikt <vladoss339@gmail.com>
2024-03-03 12:57:43 +0100

    fixed .yml file

--- src/main/java/dev/tiltrikt/web3j/spring/boot/starter/configuration/StarterWeb3jProperties.java
+++ src/main/java/dev/tiltrikt/web3j/spring/boot/starter/configuration/StarterWeb3jProperties.java
@@ -13,9 +13,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StarterWeb3jProperties {
 
-  @NotNull String network;
+  @NotNull String network = "https://testnet.ton.dev";
 
-  @NotNull String contract;
+  @NotNull String contract = "0xAD4AdD81B11b4951c87bdb2eb0F4aC8e0fE06712";
 
-  int decimals;
+  int decimals = 18;
 }
